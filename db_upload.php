962f9ab86e379511c54556c47851d89e94f9e68e
Johann Danner <johann.danner@gmail.com>
2019-12-27 21:22:51 +0100

    passt

--- db_upload.php
+++ db_upload.php
@@ -6,26 +6,9 @@ include ("inc/config.inc.php");
 #echo "Listeners Current:"; 
 $str = file_get_contents('http://207.180.205.39/api/nowplaying/blechradio1/');
 $listeners_current = json_decode($str, true);
-# echo $listeners_current['listeners']['current'];
 $listeners_current_fertig = $listeners_current['listeners']['current'];
-# echo $listeners_current_fertig;
 ?>
-<br>
- 
-<?php
-#echo "Listeners Total:";
-$str = file_get_contents('http://207.180.205.39/api/nowplaying/blechradio1/');
-$listeners_total = json_decode($str, true);
-# echo $listeners_total['listeners']['total'];
-?>
-<br>
 
-<?php
-#echo "Listeners Unique:";
-$str = file_get_contents('http://207.180.205.39/api/nowplaying/blechradio1/');
-$listeners_unique= json_decode($str, true);
-# echo $listeners_unique['listeners']['unique'];
-?>
 
 <?php 
 $statement = $pdo->prepare("INSERT INTO listeners (current) VALUES ('$listeners_current_fertig')");
--- li_peak_upload.php
+++ li_peak_upload.php
@@ -4,8 +4,10 @@ include ("inc/config.inc.php");
 
 #### Zwischensumme lesen
 $zwischensumme = file_get_contents("data/zwischensumme.txt");
+?>
 
-<?php 
+
+<?php
 $statement = $pdo->prepare("INSERT INTO listeners_peak (listeners) VALUES ('$zwischensumme')");
 $statement->execute(array('$zwischensumme'));   
 ?>
